7d42cd1ce426f605f5f253943cc193505f89e496
anatra97 <36736119@qq.com>
2020-08-21 17:01:16 +0800

    seata

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/wyl/sc/service/PaymentService.java
@@ -0,0 +1,14 @@
+package com.wyl.sc.service;
+
+import com.wyl.sc.entities.CommonResult;
+import com.wyl.sc.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "nacos-payment-provider",fallback = PaymentFallbackService.class)
+public interface PaymentService
+{
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id);
+}
